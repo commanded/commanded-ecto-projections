cc584bf9ac29c30be8860afc412ddb3e3d90ac41
Sascha Wolf <swolf.dev@gmail.com>
2017-09-29 10:41:01 +0200

    Migrations: Add a ProjectionVersion for a different schema prefix

---
+++ priv/repo/migrations/20170929080308_create_projection_version_with_prefix.exs
@@ -0,0 +1,20 @@
+defmodule Commanded.Projections.Repo.Migrations.CreateProjectionVersionWithPrefix do
+  use Ecto.Migration
+
+  def up do
+    execute "CREATE SCHEMA test"
+
+    create table(:projection_versions, primary_key: false, prefix: "test") do
+      add :projection_name, :text, primary_key: true
+      add :last_seen_event_number, :bigint
+
+      timestamps()
+    end
+  end
+
+  def down do
+    drop table(:projection_versions, prefix: "test")
+
+    execute "DROP SCHEMA test"
+  end
+end
